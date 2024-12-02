297175cc91e415b09973c64a53058d3b24d6a31c
tlebryk <tlebryk02@gmail.com>
2024-12-01 22:41:10 -0500

    uq init

--- ie_uq/uq_utils.py
+++ ie_uq/uq_utils.py
@@ -1,26 +1,46 @@
 import torch
 
 
-def calculate_perplexity_raw(chat, tokenizer, model):
-    # Tokenize the full input
-    chat_templated = tokenizer.apply_chat_template(
-        chat, tokenize=True, add_generation_prompt=True, return_tensors="pt"
-    )
-    # Get the length of the assistant message at the end of the inputs
 
-    outputs = model(chat_templated, labels=chat_templated)
-    # total_loss = outputs.loss.item() * chat_templated.size(1)  # Total loss over all tokens
-    logits = (
-        outputs.logits
-    )  # [:, len(prompt_ids[0])-1:-1, :]  # Only completion logits, excluding prompt
-    completion_ids = (
-        chat_templated  # [:, len(prompt_ids[0]):]  # Only completion token IDs
-    )
+def calculate_perplexity_raw(text, tokenizer, model):
+    # assume no gradients
+    with torch.no_grad():
+        # Tokenize the full input
+        # Get the length of the assistant message at the end of the inputs
 
-    # Calculate log probabilities for each token in the completion
-    log_probs = torch.nn.functional.log_softmax(logits, dim=-1)
-    completion_log_probs = log_probs.gather(2, completion_ids.unsqueeze(-1)).squeeze(-1)
-    avg_log_likelihood = -completion_log_probs.mean().item()
-    perplexity = torch.exp(torch.tensor(avg_log_likelihood)).item()
+        outputs = model(text, labels=text)
+        # just use loss 
+        perplexity = torch.exp(outputs.loss)
+        
+        # logits = outputs.logits  # [:, len(prompt_ids[0])-1:-1, :]  # Only completion logits, excluding prompt
+        # completion_ids = chat_templated  # [:, len(prompt_ids[0]):]  # Only completion token IDs
+
+        # # Calculate log probabilities for each token in the completion
+        # log_probs = torch.nn.functional.log_softmax(logits, dim=-1)
+        # completion_log_probs = log_probs.gather(2, completion_ids.unsqueeze(-1)).squeeze(-1)
+        # avg_log_likelihood = -completion_log_probs.mean().item()
+        # perplexity = torch.exp(torch.tensor(avg_log_likelihood)).item()
+
+    return perplexity
+
+
+def calculate_perplexity_raw_batch(texts, tokenizer, model):
+    # assume no gradients
+    with torch.no_grad():
+        # Tokenize the full input
+        # Get the length of the assistant message at the end of the inputs
+
+        outputs = model(texts, labels=texts)
+        # just use loss 
+        perplexity = torch.exp(outputs.loss)
+        
+        # logits = outputs.logits  # [:, len(prompt_ids[0])-1:-1, :]  # Only completion logits, excluding prompt
+        # completion_ids = chat_templated  # [:, len(prompt_ids[0]):]  # Only completion token IDs
+
+        # # Calculate log probabilities for each token in the completion
+        # log_probs = torch.nn.functional.log_softmax(logits, dim=-1)
+        # completion_log_probs = log_probs.gather(2, completion_ids.unsqueeze(-1)).squeeze(-1)
+        # avg_log_likelihood = -completion_log_probs.mean().item()
+        # perplexity = torch.exp(torch.tensor(avg_log_likelihood)).item()
 
     return perplexity
