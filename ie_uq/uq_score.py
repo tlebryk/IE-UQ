6d9add4ba54f5cd8027e94b8460707a4ef1bda4b
tlebryk <tlebryk02@gmail.com>
2024-12-01 22:46:44 -0500

    add score

---
+++ ie_uq/uq_score.py
@@ -0,0 +1,86 @@
+# %%
+# import langchain
+from datasets import load_dataset
+from transformers import (
+    AutoTokenizer,
+    AutoModelForCausalLM,
+    AutoConfig,
+    pipeline,
+)
+import os
+import torch
+import time
+
+from typing import Optional, Union
+
+
+from ie_uq.config_utils import ConfigLoader
+from ie_uq.data_preprocess import DataPreprocessOai
+from ie_uq.data_load import DataLoad
+from ie_uq import uq_utils
+from trl import SFTTrainer, setup_chat_format, DataCollatorForCompletionOnlyLM
+
+
+def main(
+    model_id: str = "meta-llama/Llama-3.2-1B-Instruct",
+    dataset_path: str = "https://raw.githubusercontent.com/tlebryk/IE-UQ/refs/heads/develop/data/cleaned_dataset.jsonl",
+    mode: str = "synth_span",
+    output_dir: str = None,
+    bnb_dict: Optional[Union[str, dict]] = None,
+    peft_dict: Optional[Union[str, dict]] = None,
+    sft_dict: Optional[Union[str, dict]] = None,
+    model_dict: Optional[Union[str, dict]] = None,
+    generation_dict: Optional[Union[str, dict]] = None,
+) -> None:
+    if not output_dir:
+        # use current datetime
+        output_dir = f"outputs/{time.strftime('%Y-%m-%d_%H-%M-%S')}"
+    os.makedirs(output_dir, exist_ok=True)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # BitsAndBytesConfig
+    # TODO: change to object grouping configs?
+    bnb_config = ConfigLoader.load_bnb(bnb_dict)
+    peft_config = ConfigLoader.load_peft(peft_dict)
+    sft_config = ConfigLoader.load_sft(sft_dict, output_dir=output_dir, device=device)
+    model_dict = ConfigLoader.load_model_dict(
+        model_dict, device=device, bnb_config=bnb_config
+    )
+    model_config = AutoConfig.from_pretrained(model_id)
+    generation_config = ConfigLoader.load_generation(generation_dict, model_config)
+    # TODO: load model config here.
+    #  load data
+    # THIS PART SHOULD BE PLUGGED IN AND OUT DEPENDING ON THE DATASET
+
+    dataset = DataLoad.load(dataset_path, split="train")
+    formater = getattr(DataPreprocessOai, mode, lambda x: x)
+    dataset = dataset.map(formater, remove_columns=dataset.features, batched=False)
+    # if no mode, assume extraction.
+    # train test split dataset
+    # TODO: clean this up...
+    split_dataset = dataset.train_test_split(test_size=0.1)
+    train_dataset = split_dataset["train"]
+    test_dataset = split_dataset["test"]
+
+    # get model_config
+
+    model = AutoModelForCausalLM.from_pretrained(model_id, **model_dict)
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    model = model.eval()
+    # reset model to use default chat template
+    # tokenizer.chat_template = None
+    # model, tokenizer = setup_chat_format(model, tokenizer)
+    tokenizer.pad_token = tokenizer.eos_token
+    train_dataset = train_dataset.map(
+        lambda x: {"formated_chat":  tokenizer.apply_chat_template(x["messages"][:-1], tokenize=False, add_generation_prompt=False)} ) # maybe drop messages after this? 
+    train_dataset = train_dataset.map({
+        "perplexity": lambda x: uq_utils.calculate_perplexity_raw(x["formated_chat"], tokenizer, model)
+    })
+    
+    # save the perplexity scores 
+    train_dataset.to_json(os.path.join(output_dir, "perplexity_scores.json"))
+    # get the n highest perplexity elements
+    # n = 5
+    # perplexity_scores = train_dataset.sort("perplexity", reverse=True)
+    # print(perplexity_scores[:n])
+    # get the n highest perplexity elements
+    
